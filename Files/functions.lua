af5bd1e6c267dfa005ca0ab8d52fd66cb1974497
vcsk0 <111982858+Vcsk@users.noreply.github.com>
2023-04-29 20:11:49 +0800

    Update functions.lua

--- Files/functions.lua
+++ Files/functions.lua
@@ -1,1 +1,23 @@
-abc.createBed = function()
+local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))()
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
+
+function notif(title,text,duration)
+	title = title or "Title"
+	text = text or "Text"
+	duration = duration or 5
+	game.StarterGui:SetCore("SendNotification", {
+		Title = title;
+		Text = text;
+		Duration = duration;
+	})
+end
+
+function notif2(title,description,time)
+	title = title or "Title"
+	description = description or "Description"
+	time = time or 5
+	Notification:Notify(
+		{Title = title, Description = description},
+		{OutlineColor = Color3.fromRGB(128, 187, 219),Time = time, Type = "default"}
+	)
+end
